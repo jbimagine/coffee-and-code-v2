419b76cc9fd32df35b48ee45ff8040f6ef511c79
Joe Bates <joebates@jbimagine.com>
2019-01-26 20:47:16 -0800

    Style contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { Section, SectionHeader, NAVIGATION_HEIGHT, secondary } from '../styles/main_styles';
+import { 
+  Section, 
+  SectionContent,
+  SectionHeader, 
+  NAVIGATION_HEIGHT, 
+  secondary,
+  MainContent,
+  SecondaryContent,
+  BorderBox,
+ } from '../styles/main_styles';
 
 export default class Contact extends Component {
   render() {
     return (
       <>
-        <Section height = '100vh' theme ={secondary} color ='true'>
-        <SectionHeader>{this.props.title}</SectionHeader>
+        <Section height = '100%' theme ={secondary} color='true' clipPath='true'>
+          <SectionContent>
+            <SectionHeader>
+                Drop Me A Line!
+            </SectionHeader>
+          <BorderBox border='1px solid #000000' boxItem={`Let's Chat`}/>
+          <MainContent>
+          Want to discuss your project?  Or just want to chat?  Send me a message!
+          </MainContent>
+          <SecondaryContent>
+            Want to know more about me?  Feel free to check out my Github
+            and Linkedin pages!!
+          </SecondaryContent>
+            <BorderBox width = 'auto' border='1px solid #000000' boxItem={ 'Github' }/>
+            <BorderBox width = 'auto' border='1px solid #000000' boxItem={ 'Linkedin' }/>
+          </SectionContent>
         </Section>     
       </>
     );
